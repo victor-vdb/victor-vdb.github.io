73746dfb09741ec0b8240f4ef8ec5c6136d89f56
Victor Van den Broeck <victorvdb@outlook.com>
2022-03-06 19:54:37 +0100

    a = this

--- Ink/ink_demo.js
+++ Ink/ink_demo.js
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 (function() {
+    var a = this;
     var b = function() {
         var c = function(d) {
             var test = "string" === typeof STATIC_JS_PREFIX ? STATIC_JS_PREFIX + (STATIC_JS_PREFIX.match(/\/$/) ? "" : "/") + d : d;
